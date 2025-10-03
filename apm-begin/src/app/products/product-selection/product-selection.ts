0f39f131f68f0455e5420c0d1d8262b8ab5500c1
Brett Cunningham <bwcunninghamii@gmail.com>
2025-10-03 06:51:49 -0400

    creating and reading signals 01

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
+// NOTE: two way binding with ngModel in the template requires the FormsModule
 @Component({
   selector: 'app-product-selection',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-selection.html',
   styleUrl: './product-selection.css'
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
 
+  quantity: WritableSignal<number> = signal(1);
+
 }
+
