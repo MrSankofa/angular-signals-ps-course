95d18ad95051ad9e2f1c699841d4900c9b007ef0
Brett Cunningham <bwcunninghamii@gmail.com>
2025-10-03 07:30:47 -0400

    arrays in signals, event handling signals

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,5 +1,7 @@
 import {Component, signal, WritableSignal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {ProductData} from '../product-data';
+import {Product} from '../product';
 
 // NOTE: two way binding with ngModel in the template requires the FormsModule
 @Component({
@@ -12,6 +14,10 @@ export class ProductSelection {
   pageTitle = 'Product Selection';
 
   quantity: WritableSignal<number> = signal(1);
+
+  products: WritableSignal<Product[]> = signal(ProductData.products);
+
+  selectedProduct: WritableSignal<Product | undefined> = signal(undefined);
 
 }
 
