2b9a2052adf679b71e3d0efedc20e7fc8c1ac376
Brett Cunningham <bwcunninghamii@gmail.com>
2025-10-04 09:39:41 -0400

    wip http resource work to retrieve products from server

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import {httpResource} from '@angular/common/http';
+import {Product} from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private productsUrl = 'api/products';
+
+  productsResource = httpResource<Product[]>(() => this.productsUrl, { defaultValue: []});
+
+
 }
