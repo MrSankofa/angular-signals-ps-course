14d5759edca775af0038887a3b79996749c83659
Brett Cunningham <bwcunninghamii@gmail.com>
2025-10-04 09:38:14 -0400

    intro to computed and linked signals

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,23 +1,36 @@
-import {Component, signal, WritableSignal} from '@angular/core';
+import {Component, computed, inject, linkedSignal, Signal, signal, WritableSignal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {ProductData} from '../product-data';
 import {Product} from '../product';
+import {CurrencyPipe} from '@angular/common';
+import {ProductService} from '../product.service';
 
 // NOTE: two way binding with ngModel in the template requires the FormsModule
 @Component({
   selector: 'app-product-selection',
-  imports: [FormsModule],
+  imports: [FormsModule, CurrencyPipe],
   templateUrl: './product-selection.html',
   styleUrl: './product-selection.css'
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
 
-  quantity: WritableSignal<number> = signal(1);
+  // TODO
+  productService = inject(ProductService);
 
   products: WritableSignal<Product[]> = signal(ProductData.products);
 
   selectedProduct: WritableSignal<Product | undefined> = signal(undefined);
+
+  quantity: WritableSignal<number> = linkedSignal({
+    source: this.selectedProduct,
+    computation: () => {
+      return 1;
+    }
+  })
+
+  total: Signal<number> = computed(() => ((this.selectedProduct()?.price ?? 0) * this.quantity()) );
+  color: Signal<string> = computed(() => this.total() > 200 ? 'green' : 'blue');
 
   onDecrease() {
     this.quantity.update(currentQuantity => {
